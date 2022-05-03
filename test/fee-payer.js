878626c7f453378a16d72c4f83194c8027c3a8d0
tranthelam <lamtt2403@gmail.com>
2022-05-03 23:39:25 +0700

    refactor: test/fee-payer

--- test/fee-payer.js
+++ test/fee-payer.js
@@ -4,6 +4,7 @@ const { ethers } = require('ethers');
 const assert = require('assert')
 const MQTT = require('async-mqtt')
 const { wrapTx } = require('../src/usecases/fee-payer')
+const { initRedisConn, closeRedisConn } = require('../src/connections/redis')
 
 const MQTT_URI = `mqtt://${process.env.MQTT_HOST}:${process.env.MQTT_PORT}`
 const MQTT_TOPIC = process.env.TOPIC
@@ -13,97 +14,6 @@ const provider = new ethers.providers.JsonRpcProvider(RPC)
 const nocoin = new ethers.Wallet.createRandom().connect(provider)
 const dummy = new ethers.Wallet.createRandom()
 
-
-// describe('mqtt connection', () => {
-//     let client
-    
-//     afterEach(() => {
-//         if (client) {
-//             client.on('close', () => {
-//                 console.log('Connection closed')
-//             })
-
-//             client.end(true)
-//             client = undefined
-//         }
-//     })
-  
-//     it('should be able to connect to the broker', (done) => {
-//         client = MQTT.connect(MQTT_URI) 
-
-//         client.on('connect', () => {
-//             assert.equal(client.connected, true, 'Connection failed')
-//             client.subscribe(MQTT_TOPIC)
-//             client.publish(MQTT_TOPIC, "1")
-//         })
-
-//         client.on('message', (topic, message) =>{
-//             assert.equal(parseInt(message), "1", "Wrong message")
-//             done()
-//         })
-//     })
-// })
-
-
-// describe('Fee payer', () => {
-//     let chainId
-//     let client
-
-//     before( async () => {
-//         client = await MQTT.connect(MQTT_URI) 
-//         client.on('connect', async () => {
-//             assert.equal(client.connected, true, 'Connection failed')
-//             await client.subscribe(`${MQTT_TOPIC}/#`)
-//         })
-
-//         chainId = await provider.getNetwork().chainId
-       
-//     })
-    
-//     after( async () => {
-//         if (client) {
-//             client.on('close', () => {
-//                 console.log('Connection closed')
-//             })
-
-//             client.end(true)
-//             client = undefined
-//         }
-//     })
-    
-//     it("tx fee payed", async () => {
-
-//         await assert.rejects(
-//             nocoin.sendTransaction({ to: dummy.address, gasLimit: 21000, }),
-//             { reason: 'insufficient funds for intrinsic transaction cost' },
-//         )
-
-//         const nonce = await nocoin.getTransactionCount('pending')
-
-//         const tx = {
-//             chainId,
-//             to: dummy.address,
-//             gasLimit: 21000,
-//             gasPrice: 0,
-//             nonce,
-//         }
-
-//         const rawSignedTx = await nocoin.signTransaction(tx)
-
-//         client.publish(`${MQTT_TOPIC}/${nocoin.address}`, rawSignedTx)
-        
-//         client.on("message", async (topic, message) => {
-//             const [isValidSchema, isFeePayer] = await feePayer(message.toString())
-//             assert.equal(isValidSchema, true, 'Wrong format schema tx')
-//             assert.equal(isFeePayer, true, 'Payfor function failed')
-
-//             const newNonce = await nocoin.getTransactionCount('pending')
-//             assert.equal(newNonce, nonce + 1, 'Nocoin tx failed')
-//         })
-//     })
-// })
-
-
 describe("Integration test with chain", () => {
     let chainId
     let client
@@ -112,15 +22,13 @@ describe("Integration test with chain", () => {
         client = await MQTT.connect(MQTT_URI) 
         client.on('connect', async () => {
             assert.equal(client.connected, true, 'Connection failed')
-            console.log(`topic ${MQTT_TOPIC}/#`)
-            await client.subscribe(`${MQTT_TOPIC}/#`)
         })
 
-        
         chainId = await provider.getNetwork()
         chainId = chainId.chainId
         console.log(`ChainID ${JSON.stringify(chainId)}`)
-       
+
+        await initRedisConn()
     })
     
     after( async () => {
@@ -132,43 +40,53 @@ describe("Integration test with chain", () => {
             client.end(true)
             client = undefined
         }
+
+        await closeRedisConn()
     })
 
-    it("tx fee payed", async (done) => {
-        await assert.rejects(
-            nocoin.sendTransaction({ to: dummy.address, gasLimit: 21000, }),
-            { reason: 'insufficient funds for intrinsic transaction cost' },
-        )
+    it("tx fee payed", () => {
+        return new Promise(async (resolve, reject) => {
 
-        client.on("message", async (topic, message) => {
-            console.log(`Topic : ${topic} Message: ${message}`)
-            const [isValidSchema, isFeePayer] = await wrapTx(message)
+            await client.subscribe(`${MQTT_TOPIC}/#`)
+
+            await assert.rejects(
+                nocoin.sendTransaction({ to: dummy.address, gasLimit: 21000, }),
+                { reason: 'insufficient funds for intrinsic transaction cost' },
+            )
+
+            client.on("message", async (topic, message) => {
+
+                try {
+                    const [isValidSchema, isFeePayer] = await wrapTx(message)
+                    assert.equal(isValidSchema, true, 'Wrong format schema tx')
+                    assert.equal(isFeePayer, true, 'Payfor function failed')
+                    
+                    const newNonce = await nocoin.getTransactionCount('pending')
+                    assert.equal(newNonce, nonce + 1, 'Nocoin tx failed')
+
+                    resolve()
+                } catch (err) {
+                    console.log(err)
+                    reject()
+                }
+                
+            })
             
-            assert.equal(isValidSchema, true, 'Wrong format schema tx')
-            assert.equal(isFeePayer, true, 'Payfor function failed')
-            console.log(`Valid Topic : ${isValidSchema} Message: ${isValidSchema}`)
-            try {
-                const newNonce = await nocoin.getTransactionCount('pending')
-                assert.equal(newNonce, nonce + 1, 'Nocoin tx failed')
-            } catch (err) {
-                console.log(err)
+            const nonce = await nocoin.getTransactionCount('pending')
+            
+            const tx = {
+                chainId,
+                to: dummy.address,
+                gasLimit: 21000,
+                gasPrice: 0,
+                nonce,
+                type: 0
             }
-            done()
+            
+            await nocoin.sendTransaction(tx)
+
         })
-
-
-        const nonce = await nocoin.getTransactionCount('pending')
-
-        const tx = {
-            chainId,
-            to: dummy.address,
-            gasLimit: 21000,
-            gasPrice: 0,
-            nonce,
-            type: 0
-        }
-
-        await nocoin.sendTransaction(tx)
 
     })
 })
+
