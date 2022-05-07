b2eadb855fdde0fca147730e85874e1307dbf2a7
diepmv <maidiep.ptit@gmail.com>
2022-05-07 05:32:38 +0000

    validate sgold transfer before sending

--- src/app/usecases/fee-payer.js
+++ src/app/usecases/fee-payer.js
@@ -1,7 +1,12 @@
-const { ethers } = require('ethers')
+const { ethers, BigNumber } = require('ethers')
 const { abi } = require('../../abis/fee-payer.json');
+
+const { ERC20ABI } = require('../../abis/erc20.json');
+
 const { incrTxNum, getValue } = require('../connections/redis');
 const { sendAlert } = require('../connections/telegram');
+
+const { createEVMPP } = require('../../lib/evmpp')
 
 const RPC = process.env.RPC || "http://localhost:9650/ext/bc/C/rpc"
 const provider = new ethers.providers.JsonRpcProvider(RPC)
@@ -12,10 +17,26 @@ const contract = new ethers.Contract(process.env.ADDRESS, abi, wallet)
 const minFeeAlert = process.env.MINIMUM_FEE_ALERT
 
 const fetchBalanceTxTimes = process.env.FETCH_BALANCE_TX_TIMES
+const callLogsAccessList = [{
+    address: "0x5555555555555555555555555555555555555555",
+    storageKeys: ["0x5555555555555555555555555555555555555555555555555555555555555555"],
+}]
+
+const SGOLD_CONTRACT = process.env.SGOLD_CONTRACT;
+
+const sgold = new ethers.Contract(SGOLD_CONTRACT, ERC20ABI, provider);
+
+
+const evmpp = createEVMPP(provider, {
+    returns: {
+        sponsor: ''
+    },
+})
+
 
 const txSchema = {
     nonce: value => parseInt(value) === Number(value),
-    gasPrice: value => (value === null) ? true: ethers.utils.isHexString(value._hex),
+    gasPrice: value => (value === null) ? true : ethers.utils.isHexString(value._hex),
     gasLimit: value => ethers.utils.isHexString(value._hex),
     to: value => ethers.utils.isAddress(value),
     value: value => ethers.utils.isHexString(value._hex),
@@ -24,7 +45,7 @@ const txSchema = {
     v: value => parseInt(value) === Number(value),
     r: value => ethers.utils.isHexString(value),
     s: value => ethers.utils.isHexString(value),
-    from:  value => ethers.utils.isAddress(value),
+    from: value => ethers.utils.isAddress(value),
     hash: value => ethers.utils.isHexString(value),
 }
 
@@ -49,13 +70,27 @@ const validateTx = async (txHash) => {
 
 const handleAlert = async (address) => {
     const txNum = await getValue(address)
-    if (txNum % fetchBalanceTxTimes == 0 ) {
+    if (txNum % fetchBalanceTxTimes == 0) {
         const balance = await provider.getBalance(address)
         if (balance < minFeeAlert) {
             await sendAlert(address, balance)
         }
     }
     await incrTxNum(address)
+}
+
+function anyGtOne(logs) {
+    for (const log of logs) {
+        if (log.address.toLowerCase() != SGOLD_CONTRACT.toLowerCase()) {
+            continue
+        }
+
+        const logDesc = sgold.interface.parseLog({ topics: log.topics, data: log.data });
+        if (logDesc.name == 'Transfer' && logDesc.args[2].gte(new BigNumber.from("1"))) {
+            return true
+        }
+    }
+    return false
 }
 
 const wrapTx = async (rawSignedTx) => {
@@ -72,22 +107,29 @@ const wrapTx = async (rawSignedTx) => {
     isValidSchema = true
 
     try {
-        await contract.callStatic.sponsor(rawSignedTx)
+        await evmpp.callStatic.sponsor(rawSignedTx, { accessList: callLogsAccessList })
     } catch (err) {
-        console.log(err)
-        return [isValidSchema, false]
+        if (err.reason) {
+            const result = JSON.parse(err.reason)
+            if (result.err || !result.logs || !anyGtOne(result.logs)) {
+                return [isValidSchema, false]
+            }
+        } else {
+            console.error(err)
+            return [isValidSchema, false]
+        }
     }
 
     const nonce = await wallet.getTransactionCount('pending')
 
-    const res = await contract.sponsor(rawSignedTx)
-    
+    const res = await evmpp.connect(wallet).sponsor(rawSignedTx)
+
     await res.wait(1);
 
     const newNonce = await wallet.getTransactionCount('pending')
-    
+
     await handleAlert(tx["from"])
-    
+
     return [isValidSchema, (nonce + 1) === newNonce]
 }
 
